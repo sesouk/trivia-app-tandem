c0343a55211d2db010048a4b4742a7be326ae989
Kevin Tran <sesouk@gmail.com>
2020-11-01 11:01:31 -0700

    added readme and documented code

--- README.md
+++ README.md
@@ -1,70 +1,54 @@
-# Getting Started with Create React App
+# Trivia App - Tandem
+> Purpose: Coding Challenge for Tandem's Software Engineering Apprenticeship | October 2020
+> Developed by Kevin Tran
+> Started: 10/28/2020
+> Submitted: 11/01/2020
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Getting Started
+1. Clone the repositiory
+2. cd into the directory
+3. Run yarn
+4. Run yarn start to start the development environment
+5. Open http://localhost:3000 in your browser
 
-## Available Scripts
+#### Things to Be Familiar With for This Repo
+* Arrays and loops 
+* Data manipulation 
+* Parsing JSON
+* React
+* JavaScript
+* HTML
+* CSS
+* Ternaries
 
-In the project directory, you can run:
+## Code Challenge Prompt - Tandem for 400!
+At Tandem, we love to learn and have fun and what better way to do that than to play a round of trivia. One of our favorite ways to wait while our test suite is running is to hop over to the #games channel on Slack and play a quick round of trivia. Playing trivia isn’t just a fun way to learn something new but also a great way to take a little break from a normal work task.
 
-### `yarn start`
+While we’re all knowledgeable in our own right, only one person can be crowned trivia champion with the highest score. Your goal is to create an application that others will be able to use in order to help improve their trivia skills.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Train to improve your trivia knowledge by creating your own trivia training app!
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+## Code Challenge Goal
+Your goal is to create an application that displays trivia questions with multiple-choice answers to select from.
 
-### `yarn test`
+Use creative license in terms of how you want us to see this game. At minimum, the player can view the question(s), the answer choices, the correct answer upon submission, and their score. It could be a user interface (UI), command-line-tool, etc. Feel free to use whatever framework or language you are comfortable with.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+We would also like to see a README which includes any information about how to run the code, any known issues or complexity we should look out for, and any additional features you would like to have added to make your trivia app even more awesome.
 
-### `yarn build`
+## Assumptions
+* A round of trivia has 10 Questions
+* All questions are multiple-choice questions
+* Your score does not need to update in real time
+* Results can update on form submit, button click, or any interaction you choose
+* We will provide you with the trivia data such as the questions, correct and incorrect answers via a JSON file.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Acceptance Criteria
+* A user can view questions.
+* Questions with their multiple choice options must be displayed one at a time. Questions should not repeat in a round.
+* A user can select only 1 answer out of the 4 possible answers.
+* The correct answer must be revealed after a user has submitted their answer A user can see the score they received at the end of the round
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## Future Improvements
+* User login to keep track of scores between sessions
+* Choose specific questions to review
+* Score leaderboard
--- src/components/questions.js
+++ src/components/questions.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import data from '../data/data.json'
 
 export default function App() {
-
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
+// State of the app to facilitate question and answer rendering and shuffling and flow of the web app through conditionals.
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
   const [ansArr, setAns] = useState([]);
   const [questions, setQs] = useState(data)
   const [start, setStart] = useState(false)
@@ -14,53 +14,61 @@ export default function App() {
   const [ans, setCorrectAns] = useState('')
   const [checked, setChecked] = useState(false)
   const [showQs, setShowQs] = useState(false)
-  
+
+// Retrieves the current question's answers and shuffles them when the state is changed to the next question in the application 
   useEffect(() => {
     const getAnswers = () => {
       let arr1 = []
       if (questions[currentQuestion]) {
-      arr1.push({answerText: questions[currentQuestion].correct, isCorrect: true})
-      for (let i = 0; i < questions[currentQuestion].incorrect.length; i++) {
-        arr1.push({answerText: questions[currentQuestion].incorrect[i], isCorrect: false})
-      }
-      return setAns(shuffle(arr1))
+        arr1.push({ answerText: questions[currentQuestion].correct, isCorrect: true })
+        for (let i = 0; i < questions[currentQuestion].incorrect.length; i++) {
+          arr1.push({ answerText: questions[currentQuestion].incorrect[i], isCorrect: false })
+        }
+        return setAns(shuffle(arr1))
       }
     }
     getAnswers()
   }, [currentQuestion, start, questions])
 
-	const handleAnsOpt = (correct) => {
-		if (correct) {
-			setScore(score + 1)
-		}
-		setCurrentQuestion(currentQuestion + 1);
+// When a correct answer is chosen the score is updated, the current question is updated, and moved to the next question 
+  const handleAnsOpt = (correct) => {
+    if (correct) {
+      setScore(score + 1)
+    }
+    setCurrentQuestion(currentQuestion + 1);
     nextQ()
   }
 
+// Shuffles the questions and reduces the amount of questions to 10 and begins the round of trivia
   const getQs = () => {
     setQs(shuffle(data))
     setQs(questions.slice(0, 10))
     setStart(true)
   }
 
+// Shuffles a given array, used to shuffle the array of questions and answers respectively
   const shuffle = (arr) => {
     let counter = arr.length, temp, index;
     while (counter > 0) {
-        index = Math.floor(Math.random() * counter);
-        counter--;
-        temp = arr[counter];
-        arr[counter] = arr[index];
-        arr[index] = temp;
+      index = Math.floor(Math.random() * counter);
+      counter--;
+      temp = arr[counter];
+      arr[counter] = arr[index];
+      arr[index] = temp;
     }
     return arr;
   }
 
+// Determines if the answer chosen is correct, gets the correct answer for display, and allows submission of the answer
   const handleChange = (isCorrect, arr) => {
     setCorrect(isCorrect)
     getCorrectAns(arr)
     setChecked(true)
   }
 
+// Gets the whether the answer is correct or not, if an answer is selected the answer will be submitted and moved to the
+// next question while also resetting the checked state of the answer. If no answer is checked the user will be alerted to
+// select an answer before continuing
   const handleClick = (correct) => {
     if (checked) {
       handleAnsOpt(correct)
@@ -70,6 +78,8 @@ export default function App() {
     }
   }
 
+
+// Loops through the answer array to get the correct answer for display
   const getCorrectAns = (arr) => {
     for (let ansOpt of arr) {
       if (ansOpt.isCorrect) {
@@ -78,8 +88,10 @@ export default function App() {
     }
   }
 
+// Moves to the next question 
   const nextQ = () => setNextQ(!q)
 
+// Resets all state of the application to restart the round of trivia
   const restart = () => {
     setCurrentQuestion(0)
     setShowScore(false)
@@ -92,77 +104,79 @@ export default function App() {
     setCorrectAns('')
   }
 
+// Shows the list of all available questions
   const show = () => setShowQs(!showQs)
 
+// Loops through the array of questions and displays them for the user to review
   const allQs = data.map((e, i) => {
     return <div key={i}>
       {i + 1}: {e.question}
     </div>
   })
 
-	return (
+  return (
     showQs
-    ?
-    <div className='questions'>
-      <span onClick={() => show()}>X</span>
-      {allQs}
-    </div>
-    :
-		<div className='app'>
-      { !start
-        ?
-        <>
-        <button onClick={() => getQs()} style={{marginRight: `8px`}}>Click to start the trivia!</button>
-        <button onClick={() => show()} style={{marginLeft: `8px`}}>Click to review the questions!</button>
-        </>
-        :
-        showScore
-        ?
-        (
-          <>
-          <div className='score-section'>
-					  You scored {score} out of {questions.length}
-				  </div>
-          <button onClick={() => restart()}>Reset</button>
-          </>
-        )
-        :
-        q
-        ?
-        <>
-        <div className='score-section'>
-        <div>The correct answer is: {ans}</div>
-        </div>
-        {
-          currentQuestion !== 10
+      ?
+      <div className='questions'>
+        <span onClick={() => show()}>X</span>
+        {allQs}
+      </div>
+      :
+      <div className='app'>
+        {!start
           ?
-          <button onClick={() => nextQ()}>Next Question</button>
+          <>
+            <button onClick={() => getQs()} style={{ marginRight: `8px` }}>Click to start the trivia!</button>
+            <button onClick={() => show()} style={{ marginLeft: `8px` }}>Click to review the questions!</button>
+          </>
           :
-          <button onClick={() => setShowScore(true)}>Show My Score!</button>
-        }
-        </>
-        :
-        <>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
-						</div>
-						<div className='question-text'>{questions[currentQuestion].question}</div>
-					</div>
-					<div className='answer-section'>
-            {
-              ansArr.map((ansOpt) => (
-                <div key={ansOpt.answerText} className='answer-option'>
-                <input type='radio' name='answers' id={ansOpt.answerText} onChange={() => handleChange(ansOpt.isCorrect, ansArr)}/>
-                <label htmlFor={ansOpt.answerText}>{ansOpt.answerText}</label>
+          showScore
+            ?
+            (
+              <>
+                <div className='score-section'>
+                  You scored {score} out of {questions.length}
                 </div>
-              )
-              )
-            }
-            <button onClick={() => handleClick(correct)}>Submit</button>
-					</div>
-				</>
-      }
-		</div>
-	);
+                <button onClick={() => restart()}>Reset</button>
+              </>
+            )
+            :
+            q
+              ?
+              <>
+                <div className='score-section'>
+                  <div>The correct answer is: {ans}</div>
+                </div>
+                {
+                  currentQuestion !== 10
+                    ?
+                    <button onClick={() => nextQ()}>Next Question</button>
+                    :
+                    <button onClick={() => setShowScore(true)}>Show My Score!</button>
+                }
+              </>
+              :
+              <>
+                <div className='question-section'>
+                  <div className='question-count'>
+                    <span>Question {currentQuestion + 1}</span>/{questions.length}
+                  </div>
+                  <div className='question-text'>{questions[currentQuestion].question}</div>
+                </div>
+                <div className='answer-section'>
+                  {
+                    ansArr.map((ansOpt) => (
+                      <div key={ansOpt.answerText} className='answer-option'>
+                        <input type='radio' name='answers' id={ansOpt.answerText} onChange={() => handleChange(ansOpt.isCorrect, ansArr)} />
+                        <label htmlFor={ansOpt.answerText}>{ansOpt.answerText}</label>
+                      </div>
+                    )
+                    )
+                  }
+                  <button onClick={() => handleClick(correct)}>Submit</button>
+                </div>
+              </>
+        }
+      </div>
+  );
 }
